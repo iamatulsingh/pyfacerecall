ffdf3621704df1321ba6f0f280cabfc016f11ec9
Atul Singh <atulsingh0401@gmail.com>
2022-11-20 20:17:12 +0100

    feat: initial code

--- test.py
+++ test.py
@@ -5,8 +5,3 @@ if __name__ == '__main__':
     model_path = "model"
     image_path = 'test.jpeg'
     face_recognition = FaceRecognition("./dataset/training", "./dataset/testing")
-    face_recognition.training()
-    face_recognition.save_model(model_path)
-    model = FaceRecognition.load_saved_model(model_path)
-    k, result = FaceRecognition.model_prediction(image_path, model_path)
-    print(f"detected class is {k} and prediction percentage is {result}")
