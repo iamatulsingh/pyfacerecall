085d6a75d3c47af153c565990e24bc3a9796be87
Atul Singh <atulsingh0401@gmail.com>
2022-11-23 22:47:39 +0100

    fix: README.md and test.py file

--- test.py
+++ test.py
@@ -10,5 +10,5 @@ if __name__ == '__main__':
     # face_recognition = FaceRecognition('./dataset/training', './dataset/testing')
     # after you have your model
     face_recognition = FaceRecognition(number_of_classes=2)
-    print(face_recognition.model_prediction(image_path, model_path, False))
-    # face_recognition.realtime_prediction(video_path, model_path)
+    # make need_cropping=True if you need to detect and cut the face before passing it to model
+    print(face_recognition.model_prediction(image_path, model_path, need_cropping=False))
