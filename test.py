daaf8746bafa0d7b38674f125cb240f9507ff8a2
Atul Singh <atulsingh0401@gmail.com>
2022-11-23 21:27:47 +0100

    fix: remove required param for dataset

--- test.py
+++ test.py
@@ -4,4 +4,7 @@ from pyfacerecall import FaceRecognition
 if __name__ == '__main__':
     model_path = "model"
     image_path = 'test.jpeg'
+    # for training
     face_recognition = FaceRecognition("./dataset/training", "./dataset/testing")
+    # after you have your model
+    # face_recognition = FaceRecognition()
